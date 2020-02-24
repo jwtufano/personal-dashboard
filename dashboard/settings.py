a18e1be9a8c977c6855b5605acd80b5437d4e706
awh4kc <awh4kc@virginia.edu>
2020-02-24 19:49:07 +0000

    updated config file and added postgres, heroku, and logging to settings

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+import logging.config
+import config.config as CONFIG
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8=5&jz29rqci3$n-+9#i=s32e&ls%47m!0rmkju!4j=_18yftb'
+SECRET_KEY = CONFIG.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.0.0.1', 'localhost', 'pdnj.herokuapp.com', 'cs3240.singlesourceoftruth.xyz']
 
 
 # Application definition
@@ -43,6 +46,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
     'backend',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -79,13 +83,48 @@ WSGI_APPLICATION = 'dashboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'HEROKU' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': CONFIG.HEROKU_NAME,
+            'USER': CONFIG.HEROKU_USER,
+            'PASSWORD': CONFIG.HEROKU_PASSWORD,
+            'HOST': CONFIG.HEROKU_HOST,
+            'PORT': CONFIG.HEROKU_PORT,
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
+# Logging Configuration
+
+# Clear previous config
+LOGGING_CONFIG = None
+
+# Get loglevel from env
+LOGLEVEL = os.getenv("DJANGO_LOGLEVEL", "info").upper()
+
+logging.config.dictConfig(
+    {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "console": {
+                "format": "%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s",
+            },
+        },
+        "handlers": {
+            "console": {"class": "logging.StreamHandler", "formatter": "console",},
+        },
+        "loggers": {"": {"level": LOGLEVEL, "handlers": ["console",],},},
+    }
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -111,7 +150,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
@@ -124,6 +163,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'collected-static/'
 
 AUTH_USER_MODEL = 'backend.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
@@ -138,3 +179,7 @@ SITE_ID = 1
 
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_USERNAME_REQUIRED = False
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+django_heroku.settings(locals())
