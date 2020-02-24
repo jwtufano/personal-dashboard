3e0aea4d1bcf4deabb57f6143913b03921872300
Jason Tufano <jt2ef@virginia.edu>
2020-02-24 13:49:05 -0500

    Added very basic structure for many models (probably still missing some though)

--- backend/models.py
+++ backend/models.py
@@ -5,9 +5,10 @@ from django.utils import timezone
 # Create your models here.
 
 class Task(models.Model):
-	task_name = models.CharField(max_length=200)
-	pub_date = models.DateTimeField('date due')
-	task_description = models.CharField(max_length=500)
+	task_name = models.CharField(max_length = 100)
+	due_date = models.DateTimeField('date due')
+	task_description = models.CharField(max_length = 500)
+	subtasks = {}
 
 	def __str__(self):
 		return self.task_name + ":" + self.task_description
@@ -15,7 +16,10 @@ class Task(models.Model):
 
 class TaskList(models.Model):
 	Tasks = {}
-	list_name = models.CharField(max_length =200)
+	tasklist_name = models.CharField(max_length = 100)
+	tasklist_description = models.CharField(max_length = 500)
+	#Considering using https://github.com/fabiocaccamo/django-colorfield for color
+	tasklist_color = '#0000FF'
 
 	def __str__(self):
 		return self.list_name
@@ -28,3 +32,36 @@ class TaskList(models.Model):
 	def delTask(self, name):
 		del self.Tasks[name]
 		return self.Tasks
+
+class TaskListGroup(models.Model):
+	TaskLists = {}
+	tasklist_group_name = models.CharField(max_length = 100)
+	#Considering using https://github.com/fabiocaccamo/django-colorfield for color
+	tasklist_group_color = models.CharField(max_length = 7)
+
+class Event(models.Model):
+	date_time = models.DateTimeField('Event date')
+	event_name = models.CharField(max_length = 100)
+	event_description = models.CharField(max_length = 500)
+	event_color = '#0000FF'
+
+class EventList(models.Model):
+	events = {}
+	eventlist_name = models.CharField(max_length = 100)
+	eventlist_description = models.CharField(max_length = 500)
+
+class Course(models.Model):
+	course_name = models.CharField(max_length = 100)
+	grade_categories = {}
+
+class GradeCategory(models.Model):
+	parent_course_name = models.CharField(max_length = 100)
+	category_name = models.CharField(max_length = 100)
+	weight = models.DecimalField
+	grades = {}
+
+class Grade(models.Model):
+	parent_course_name = models.CharField(max_length = 100)
+	parent_category_name = models.CharField(max_length = 100)
+	earned_points = models.IntegerField()
+	possible_points = models.IntegerField()
