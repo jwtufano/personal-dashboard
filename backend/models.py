899304ca62881ccb4c73f7730421f0a3ccc81754
Jason Tufano <jt2ef@virginia.edu>
2020-03-02 13:45:59 -0500

    Did some model stuff and very few unit tests

--- backend/models.py
+++ backend/models.py
@@ -7,65 +7,87 @@ from django.utils import timezone
 class CustomUser(AbstractUser):
     pass
 
+def tomorrow():
+	return timezone.now() + timezone.timedelta(days=1)
 
 class Task(models.Model):
-	task_name = models.CharField(max_length = 100)
-	due_date = models.DateTimeField('date due', default=timezone.now() + timezone.timedelta(days=1))
-	task_description = models.CharField(max_length = 500)
-	subtasks = {}
+	DEFAULT_PK = 1
+	task_name = models.CharField(max_length = 100, default='Task')
+	task_description = models.CharField(max_length = 500, default='Task description')
+	task_due_date = models.DateTimeField(default=tomorrow)
+	parent_task = models.ForeignKey('self', default=DEFAULT_PK, on_delete=models.CASCADE)
 
 	def __str__(self):
-		return self.task_name + ":" + self.task_description
-
+		return self.task_name + ": " + self.task_description
 
 class TaskList(models.Model):
-	Tasks = {}
-	tasklist_name = models.CharField(max_length = 100)
-	tasklist_description = models.CharField(max_length = 500)
+	tasklist_name = models.CharField(max_length = 100, default='Task List')
+	tasklist_description = models.CharField(max_length = 500, default='A list of tasks.')
+	tasks = models.ManyToManyField(Task)
 	#Considering using https://github.com/fabiocaccamo/django-colorfield for color
-	tasklist_color = '#0000FF'
+	#tasklist_color = '#0000FF'
 
 	def __str__(self):
-		return self.list_name
+		return self.tasklist_name
+	
 	#Add a task into the tasklist
 	def addTask(self, task):
-		self.Tasks[task.task_name] = task
+		self.tasks.add(task)
 		return self.Tasks
 
 	#Delete a task from the tasklist
-	def delTask(self, name):
-		del self.Tasks[name]
+	def delTask(self, task):
+		self.tasks.remove(task)
 		return self.Tasks
 
 class TaskListGroup(models.Model):
+	tasklist_group_name = models.CharField(max_length = 100, default='TaskList Group')
 	TaskLists = {}
-	tasklist_group_name = models.CharField(max_length = 100)
 	#Considering using https://github.com/fabiocaccamo/django-colorfield for color
-	tasklist_group_color = models.CharField(max_length = 7)
+	#tasklist_group_color = models.CharField(max_length = 7)
+
+	def __str__(self):
+		return self.tasklist_group_name
 
 class Event(models.Model):
-	date_time = models.DateTimeField('Event date')
-	event_name = models.CharField(max_length = 100)
-	event_description = models.CharField(max_length = 500)
-	event_color = '#0000FF'
+	event_name = models.CharField(max_length = 100, default='Event')
+	event_description = models.CharField(max_length = 500, default='Event description')
+	date_time = models.DateTimeField('event date and time', default=timezone.now)
+	#event_color = '#0000FF'
+
+	def __str__(self):
+		return self.event_name + ': ' + self.date_time
 
 class EventList(models.Model):
-	events = {}
 	eventlist_name = models.CharField(max_length = 100)
 	eventlist_description = models.CharField(max_length = 500)
+	events = {}
+
+	def __str__(self):
+		return self.eventlist_name + ': ' + self.eventlist_description
 
 class Course(models.Model):
 	course_name = models.CharField(max_length = 100)
 	grade_categories = {}
 
+	def __str__(self):
+		return self.course_name
+
 class GradeCategory(models.Model):
-	parent_course_name = models.CharField(max_length = 100)
+	parent_course = models.ForeignKey(Course, on_delete=models.CASCADE)
 	category_name = models.CharField(max_length = 100)
 	weight = models.DecimalField
 	grades = {}
 
+	def __str__(self):
+		return self.category_name + ': ' + weight + '\%'
+
 class Grade(models.Model):
-	parent_course_name = models.CharField(max_length = 100)
-	parent_category_name = models.CharField(max_length = 100)
+	parent_category = models.ForeignKey(GradeCategory, on_delete=models.CASCADE)
+	grade_name = models.CharField(max_length = 100, default='Grade')
 	earned_points = models.IntegerField()
 	possible_points = models.IntegerField()
+
+	def __str__(self):
+		return parent_category.category_name + ' - ' + self.grade_name + ': ' \
+			   + self.earned_points + '/' + self.possible_points
--- backend/tests.py
+++ backend/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from backend.models import Task, TaskList, TaskListGroup, Event, EventList, \
+                           Course, GradeCategory, Grade
 
-# Create your tests here.
+class TaskTestCase(TestCase):
+    def setUp(self):
+        Task.objects.create()
+        Task.objects.create(task_name="Test Name", task_description="Test description")
+
+    def test_default_task_name_setup(self):
+        default = Task.objects.get(id=1)
+        self.assertEqual(default.task_name, "Task")
+    
+    def test_default_task_description_setup(self):
+        default = Task.objects.get(id=1)
+        self.assertEqual(default.task_description, "Task description")
+    
+    def test_default_task_str(self):
+        default = Task.objects.get(id=1)
+        self.assertEqual(default.__str__(), "Task: Task description")
