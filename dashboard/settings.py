411e66a3dee412b0991a64dee0a90d5244cc5955
Liem Budzien <liempbudzien@gmail.com>
2020-03-02 14:59:53 -0500

    Bug Fixes
    - updated settings.py from "172" to "127"
    - fixed css style sheet link

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = CONFIG.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.0.0.1', 'localhost', 'pdnj.herokuapp.com', 'cs3240.singlesourceoftruth.xyz']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'pdnj.herokuapp.com', 'cs3240.singlesourceoftruth.xyz']
 
 
 # Application definition
