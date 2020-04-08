c77f68d2be6ec7dbd107016442d283d1022ec060
Jason Tufano <jt2ef@virginia.edu>
2020-04-08 07:03:32 -0400

    Simple grade calculation.

--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
+from decimal import *
+
 from django.shortcuts import render, reverse
 from django.http import HttpResponseRedirect
+from django.forms.formsets import formset_factory
 
 from models.models import TaskItem, TaskList
-from app.forms import TaskItemForm, TaskListForm
+from app.forms import TaskItemForm, TaskListForm, GradeCategoryForm
 
 # Create your views here.
 def create_list(request):
@@ -73,3 +76,30 @@ def todo(request):
     if request.user.is_authenticated:
         return render(request, 'todo.html')
     return HttpResponseRedirect(reverse('home'))
+
+def grade_calc(request):
+    GradeCalcFormSet = formset_factory(GradeCategoryForm)
+    formset = GradeCalcFormSet()
+
+    if request.method == "POST":
+        formset = GradeCalcFormSet(request.POST)
+        if formset.is_valid():
+            grade = Decimal(0)
+            for form in formset:
+                grade += form.cleaned_data['category_weight']*Decimal(form.cleaned_data['current_points_earned']/form.cleaned_data['current_points_possible'])
+            grade = round(grade, 2)
+
+            context = {
+                'grade': grade,
+            }
+
+            return render(request, 'grade_calc_results.html', context)
+
+    context = {
+        'formset': formset,
+    }
+
+    return render(request, 'grade_calc.html', context)
+
+def grade_calc_results(request):
+    pass
