88b61dd63f5a1fc3b3cf733acd1955e1eea94499
Alex Hicks <awh4kc@virginia.edu>
2020-04-08 01:17:06 -0400

    merge
    Signed-off-by: Alex Hicks <awh4kc@virginia.edu>

--- app/views.py
+++ app/views.py
@@ -7,57 +7,64 @@ from app.forms import TaskItemForm, TaskListForm
 # Create your views here.
 def create_list(request):
     # add form template
-    if request.method == 'POST':
+    if request.method == "POST":
         form = TaskListForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse("dashboard:dashboard"))
     else:
         form = TaskListForm()
-    return render(request, 'create_list_form.html', {"form": form})
+    return render(request, "create_list_form.html", {"form": form})
+
 
 def update_list(request):
     # add form template
-    if request.method == 'POST':
+    if request.method == "POST":
         form = TaskListForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse("dashboard:dashboard"))
     else:
         form = TaskListForm()
-    return render(request, 'create_list_form.html', {"form": form})
+    return render(request, "create_list_form.html", {"form": form})
+
 
 def delete_list(request):
     # if post request, remove the list using TaskList.objects.get()
     pass
+
 
 def list_lists(request):
     # return list that is TaskLists.objects.get()
     pass
 
+
 def create_item(request):
     # add form template
-    if request.method == 'POST':
+    if request.method == "POST":
         form = TaskItemForm(request.POST)
         if form.is_valid():
             return HttpResponseRedirect(reverse("dashboard:dashboard"))
     else:
         form = TaskItemForm()
-    return render(request, 'create_item_form.html', {"form": form})
+    return render(request, "create_item_form.html", {"form": form})
+
 
 def update_item(request):
     # add form template
-    if request.method == 'POST':
+    if request.method == "POST":
         form = TaskItemForm(request.POST)
         if form.is_valid():
             return HttpResponseRedirect(reverse("dashboard:dashboard"))
     else:
         form = TaskItemForm()
-    return render(request, 'create_item_form.html', {"form": form})
+    return render(request, "create_item_form.html", {"form": form})
 
-def delete_list(request):
+
+def delete_item(request):
     # if post request, remove the list using TaskList.objects.get()
     pass
+
 
 def list_items(request):
     # second parameter for TaskList and return list that is TaskItems.objects.get(TaskList=passed_list)
