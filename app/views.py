de6df0a4683c081471b189bbdfcee976d6d85751
Liem Budzien <liempbudzien@gmail.com>
2020-04-14 02:25:30 -0400

    Calendar
    - created calendar to display to-do list items based on due date

--- app/views.py
+++ app/views.py
@@ -4,12 +4,42 @@ from decimal import *
 from django.forms.formsets import formset_factory
 import requests
 
-from models.models import TaskItem, TaskList
 from app.forms import TaskItemForm, TaskListForm, GradeCategoryForm
 from weather.models import City
 from weather.forms import CityForm
 
+from datetime import datetime
+from django.http import HttpResponse
+from django.views import generic
+from django.utils.safestring import mark_safe
+from models.models import *
+from .utils import Calendar
+
 # Create your views here.
+class CalendarView(generic.ListView):
+    model = TaskItem
+    template_name = 'calendar.html' 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # use today's date for the calendar
+        d = get_date(self.request.GET.get('day', None))
+
+        # Instantiate our calendar class with today's year and date
+        cal = Calendar(d.year, d.month)
+
+        # Call the formatmonth method, which returns our calendar as a table
+        html_cal = cal.formatmonth(withyear=True)
+        context['calendar'] = mark_safe(html_cal)
+        return context
+
+def get_date(req_day):
+    if req_day:
+        year, month = (int(x) for x in req_day.split('-'))
+        return date(year, month, day=1)
+    return datetime.today()
+
 def create_list(request):
     if request.method == "POST":
         form = TaskListForm(request.POST)
