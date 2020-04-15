480d1b4d5adaff7298f94edfc9eb935166902136
Liem Budzien <liempbudzien@gmail.com>
2020-04-15 03:07:17 -0400

    Changed Form Type
    - changed from form to modelForm, updated styling

--- weather/forms.py
+++ weather/forms.py
@@ -1,5 +1,11 @@
 from django import forms
+from django.forms import ModelForm, TextInput
 from models.models import City
 
-class CityForm(forms.Form):
-    name = forms.CharField(max_length=25)
+class CityForm(ModelForm):
+    class Meta:
+        model = City
+        fields = ['name']
+        widgets = {
+            'name': TextInput(attrs={'class' : 'input', 'placeholder' : 'City Name'}),
+        } #updates the input class to have the correct Bulma class and placeholder
