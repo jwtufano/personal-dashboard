5fde8da4a3a97ea8c1861d38e418547b848d1985
Liem Budzien <liempbudzien@gmail.com>
2020-04-15 12:20:32 -0400

    Weather City Check
    - only adds valid cities to list

--- app/views.py
+++ app/views.py
@@ -212,14 +212,15 @@ def dashboard(request):
 
             city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
 
-            weather = {
-                'city' : city,
-                'temperature' : city_weather['main']['temp'],
-                'description' : city_weather['weather'][0]['description'],
-                'icon' : city_weather['weather'][0]['icon']
-            }
+            if len(city_weather) > 10:
+                weather = {
+                    'city' : city,
+                    'temperature' : city_weather['main']['temp'],
+                    'description' : city_weather['weather'][0]['description'],
+                    'icon' : city_weather['weather'][0]['icon']
+                }
 
-            weather_data.append(weather) #add the data for the current city into our list
+                weather_data.append(weather) #add the data for the current city into our list
 
         context = {'weather_data' : weather_data, 'form' : form}
         return render(request, 'dashboard.html', context) #returns the index.html template
